7d122cf9bf7367ace8ace16939597c3644c06bcc
Goldinnovation <emanuelneuenhaus@hotmail.com>
2024-08-29 11:52:01 +0200

    added a dropdown and the date section

--- component/Main_Component/exposeFilter.tsx
+++ component/Main_Component/exposeFilter.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import { View, Text, Modal, Image } from "react-native";
 import React from "react";
-import { SelectList } from "react-native-dropdown-select-list";
 import { StyleSheet } from "react-native";
 import { Button } from "react-native";
+import { Dropdown } from 'react-native-element-dropdown';
 import { TouchableOpacity } from "react-native-gesture-handler";
 
 interface state {
@@ -14,23 +14,29 @@ interface state {
 const ExploreFilter: React.FC<state> = ({ callFIlter, onClose }) => {
   const [eventType, setEventType] = useState("");
   const [togglePopUp, setTogglePopUp] = useState(false);
+  const [value, setValue] = useState(null);
+  const [isFocus, setIsFocus] = useState(false);
+
 
   // console.log(callFIlter);
 
+  const data = [
+    { label: 'Item 1', value: '1' },
+    { label: 'Item 2', value: '2' },
+    { label: 'Item 3', value: '3' },
+    { label: 'Item 4', value: '4' },
+    { label: 'Item 5', value: '5' },
+    { label: 'Item 6', value: '6' },
+    { label: 'Item 7', value: '7' },
+    { label: 'Item 8', value: '8' },
+  ];
   useEffect(() => {
     if (callFIlter) {
       setTogglePopUp(!togglePopUp);
     }
   }, []);
 
-  const data = [
-    { key: "1", value: "Mobiles" },
-    { key: "2", value: "Appliances" },
-    { key: "3", value: "Cameras" },
-    { key: "5", value: "Vegetables" },
-    { key: "6", value: "Diary Products" },
-    { key: "7", value: "Drinks" },
-  ];
+
 
   // console.log(eventType);
 
@@ -60,8 +66,8 @@ const ExploreFilter: React.FC<state> = ({ callFIlter, onClose }) => {
             >
               <View style={{
                 // backgroundColor: "orange",
-                 borderBottomWidth: 1,
-                 borderBottomColor: "rgba(255, 255, 255,0.5)",
+                //  borderBottomWidth: 1,
+                //  borderBottomColor: "rgba(255, 255, 255,0.5)",
                  padding: 3,
                  height: 77,
 
@@ -123,7 +129,7 @@ const ExploreFilter: React.FC<state> = ({ callFIlter, onClose }) => {
                     
                   }}
                 >
-                  Filter Board
+                  Transition 
                 </Text>
 
                 </View>
@@ -158,10 +164,31 @@ const ExploreFilter: React.FC<state> = ({ callFIlter, onClose }) => {
             <View
               style={{
                 // backgroundColor: "rgba(57, 135, 111,0.5)",
-                height: 330,
+                height: 400,
               }}
             >
-              {/* Event Mood - Epxlore Filter */}
+                 {/* Event Mood - Explore Filter */}
+
+              <View style={{
+                  // backgroundColor: "orange",
+                  padding: 4
+              }}>
+
+               {/* Event Mood - Physical Events Header - Explore Filter */}
+
+              <View style={{
+                // backgroundColor: "green",
+                marginBottom: 4, 
+                marginTop: 2
+              }}><Text style={{
+                color:"white",
+                fontSize: 12,
+                fontWeight: 200,
+
+              }}>Select an Event Mood </Text>
+              </View>
+
+              {/* Event Mood - Physical Events Content Area - Explore Filter */}
               <View style={{
                 // backgroundColor: "pink", 
                 height: 100,
@@ -170,6 +197,7 @@ const ExploreFilter: React.FC<state> = ({ callFIlter, onClose }) => {
                 gap: 2
 
               }}>
+                
 
 
                 {/* Event Mood - Physical Events - Explore Filter */}
@@ -300,56 +328,163 @@ const ExploreFilter: React.FC<state> = ({ callFIlter, onClose }) => {
                 </View>
                 
               </View>
+                
 
-              {/* Select EVenttype + Date - Explore filter  */}
-              <View
-                style={{
-                  marginLeft: 5,
-                  position: "relative",
-                  // top: "-20%",
-                  left: "15%",
-                }}
-              >
-
-
-                {/* <SelectList
-                  setSelected={(val: any) => setEventType(val)}
-                  data={data}
-                  save="value"
-                  placeholder="Eventtype"
-                  boxStyles={{
-                    backgroundColor: "rgba(218,218,218,0.9)",
-                    height: 32,
-                    width: 160,
-                    borderRadius: 7,
-                  }}
-                  inputStyles={{
-                    fontSize: 13,
-                    height: 20,
-                    padding: 0,
-                    // backgroundColor:  "pink",
-                    position: "relative",
-                    left: "39%",
-                    top: "0%",
-                  }}
-                  dropdownStyles={{
-                    backgroundColor: "rgba(218,218,218,0.9)",
-                    height: 200,
-                    width: 200
-                  }}
-                  dropdownItemStyles={
-                    {
-                      marginHorizontal: 10
-                    }
-                  }
-                  dropdownTextStyles={{
-                    color: "black",
-                  }}
-                /> */}
               </View>
+              
+               
+              
+            
+
+              {/* Select Eventtype + Date - Explore filter  */}
+
+
+              <View style={{
+                // backgroundColor: "green",
+                padding: 4
+              }}>
+
+               {/* Select Eventtype + Date - header -  Explore filter  */}
+
+              <View style={{
+                // backgroundColor: "green",
+                marginBottom: 4, 
+                marginTop: 2
+              }}><Text style={{
+                color:"white",
+                fontSize: 12,
+                fontWeight: 200,
+
+              }}>Select a Data and an Eventtype</Text>
+              </View>
+
+              {/* Select Eventtype + Date - Content Area -  Explore filter  */}
+
+              <View style={{
+                //  backgroundColor: "pink", 
+                 height: 100,
+                 padding: 5,
+                //  flexDirection: "row",
+                 gap: 8
+              }}>
+
+             {/* Select Date - Content Area -  Explore filter  */}
+
+                <View style={{
+                  // backgroundColor: "skyblue",
+                  height: 40,
+                  flexDirection: "row",
+                  // gap:5
+
+                }}>
+                   {/* Select Date - Icon Img-  Explore filter  */}
+                   <View style={{
+                    width: "10%",
+                    // backgroundColor: "grey",
+                    justifyContent:"center",
+                    padding: 4
+                  }}>
+                      <Image
+                      source={require("../../assets/k2.png")}
+                      style={{height: 22, width: 22}}
+                      />
+                    </View>
+                  {/* Select Date - Selector -  Explore filter  */}
+
+                  <View style={{
+                    width: "90%",
+                    // backgroundColor: "orange"
+
+                  }}>
+                    <Text>2</Text>
+                    </View>
+                  
+                </View>
+
+              {/* Select Eventtype - Content Area -  Explore filter  */}
+
+                <View style={{
+                  // backgroundColor: "purple",
+                  height: 40,
+                  flexDirection: "row",
+
+                }}>
+                  {/* Select Eventtype - Icon Img-  Explore filter  */}
+                  <View style={{
+                    width: "10%",
+                    // backgroundColor: "grey",
+                    justifyContent:"center",
+                    padding: 4
+                  }}>
+                      <Image
+                      source={require("../../assets/pz1.png")}
+                      style={{height: 22, width: 22}}
+                      />
+                    </View>
+                  {/* Select Eventtype - Selector -  Explore filter  */}
+
+                  <View style={{
+                    width: "90%",
+                    // backgroundColor: "orange",
+                    alignItems: "center",
+                    justifyContent: "center"
+
+                  }}>
+                    <Dropdown
+                style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}
+                placeholderStyle={styles.placeholderStyle}
+                selectedTextStyle={styles.selectedTextStyle}
+                inputSearchStyle={styles.inputSearchStyle}
+                itemContainerStyle={styles.itemContainerStyles}
+                containerStyle={styles.ContainerStyles}
+                iconStyle={styles.iconStyle}
+                activeColor="rgba(255, 255, 255,0.1)"
+                itemTextStyle={styles.itemTextStyle}
+                data={data}
+                // search
+                maxHeight={200}
+                labelField="label"
+                valueField="value"
+                placeholder={!isFocus ? 'Select item' : '...'}
+                searchPlaceholder="Search..."
+                value={value}
+                onFocus={() => setIsFocus(true)}
+                onBlur={() => setIsFocus(false)}
+                onChange={(item: any) => {
+                  setValue(item.value);
+                  setIsFocus(false);
+                }}
+          //       renderLeftIcon={() => (
+          //         <AntDesign
+          //           style={styles.icon}
+          //           color={isFocus ? 'blue' : 'black'}
+          //           name="Safety"
+          //           size={20}
+          //         />
+          // )}
+        />
+                   
+                    </View>
+                </View>
+
+
+              
+
+
+              </View>
+
+              </View>
+              
+
                {/* Location + Maximum Distance - Explore filter   */}
-              <View></View>
+              <View style={{
+              }}>
+
+              </View>
             </View>
+
+
+                 {/* Filter btn - Explore filter   */}
             <View
               style={{
                 // backgroundColor: "skyblue",
@@ -373,6 +508,8 @@ const ExploreFilter: React.FC<state> = ({ callFIlter, onClose }) => {
                 <Text style={{color: "white"}}>Start Filter</Text>
               </TouchableOpacity>
             </View>
+
+
           </View>
         </View>
       </Modal>
@@ -386,23 +523,85 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(0, 0, 0, 0.8)", // Semi-transparent background
       // justifyContent: 'center',
       alignItems: 'center',
-    paddingTop: 119,
+    paddingTop: 109,
   },
   popup: {
       backgroundColor: 'rgba(0, 0, 0, 0.71)',
       borderWidth:1,
       borderColor: "rgba(255, 255, 255,0.5)",
-    
     width: 350,  // flex: 1,
-     height: 500,
+     height: 570,
     padding: 10,
     borderRadius: 10,
-    elevation: 10, // For shadow on Android
-    shadowColor: "#000", // For shadow on iOS
+    elevation: 10, 
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.3,
     shadowRadius: 4,
   },
+  dropdown: {
+    height: 30,
+    width: 250,
+    borderColor: 'gray',
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    backgroundColor:  "black"//"rgba(255, 255, 255,0.8)",
+  },
+  icon: {
+    marginRight: 5,
+  },
+  label: {
+    position: 'absolute',
+    backgroundColor: 'white',
+    left: 22,
+    top: 8,
+    zIndex: 999,
+    paddingHorizontal: 8,
+    fontSize: 14,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+    color: "white"
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+    // backgroundColor:"red",
+    color: "white"
+
+    
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+    color: "white",
+    // backgroundColor:"red"
+  },
+  itemContainerStyles:{
+    // backgroundColor: "grey",
+    borderBottomWidth: 1,
+    borderBottomColor: "rgba(255, 255, 255,0.1)"
+
+   
+  },
+  ContainerStyles: {
+    borderRadius: 8,
+    marginTop: 10,
+    backgroundColor:  "rgba(18,18,18,0.98)",//"rgba(255, 255, 255,0.98)"
+    borderColor: "blue"
+  }, 
+  itemTextStyle: {
+    color: "white",
+  }, 
+  // activeColor: {
+    // backgroundColor: "pink"
+  // }
+
+
 });
 
 export default ExploreFilter;
