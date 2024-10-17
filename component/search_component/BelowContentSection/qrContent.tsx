52fd96badf95e3ab6a4a3017c64ec7720ab0394a
Goldinnovation <emanuelneuenhaus@hotmail.com>
2024-10-17 16:24:35 +0200

    implemented qr code scan option on the search page

--- component/search_component/BelowContentSection/qrContent.tsx
+++ component/search_component/BelowContentSection/qrContent.tsx
@@ -1,21 +1,147 @@
+import { View, Text, Button, TouchableOpacity,} from "react-native"
+import { CameraView, CameraType, useCameraPermissions, Camera} from 'expo-camera';
+import { forwardRef, useImperativeHandle } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+// import { CameraView, Camera } from "expo-camera/next";
+// import { CameraView, useCameraPermissions } from 'expo-camera/next'
+
+
+interface qrocdeProps{
+
+  handleUserisFound: ({type, data}: any ) => void
+
+}
+
+
+const QrCodeContent =  forwardRef<any,qrocdeProps >(({handleUserisFound},ref) => {
+    const [facing, setFacing] = useState<CameraType>('back');
+    const [permission, requestPermission] = useCameraPermissions();
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+
+
+ 
+
+      useImperativeHandle(ref, () =>({
+
+        handleAkivateScann(){
+          console.log('set Scann through to false ');
+          setScanned(false)
+        }
+      }))
 
 
 
-import { View, Text } from "react-native"
+
+    const handleBarcodeScanned =  async({ type, data }: any) => {
+      setScanned(true);
+      console.log(type);
+    
+
+      const userisFounded = await handleUserisFound({type, data})
+
+      setScanned
+
+      // alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    };
+
+    if (!permission) {
+        // Camera permissions are still loading.
+        return <View />;
+      }
 
 
-const QrCodeContent = () => {
+    
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={{
+        flex: 1,
+        justifyContent: 'center',
+      }}>
+        <Text style={{
+             textAlign: 'center',
+             paddingBottom: 10,
+        }}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
 
+ 
     return(
         <View style={{
-            backgroundColor: "pink",
+            // backgroundColor: "pink",
             width: "100%", 
-            height: "100%"
-        }}>
-            <Text>qr</Text>
-        </View> 
+            height: "100%",
+            borderRadius: 4,
+            display: "flex", 
+            // justifyContent: "center"
+            // alignItems:"center"
 
-    )
-}
+        }}>
+
+        {
+          !scanned 
+          &&
+          
+           <View>
+              <CameraView style={{
+                  width: "100%",
+                  height: "80%", 
+                  // borderRadius: 40
+
+              }}
+              onBarcodeScanned={scanned ? undefined : handleBarcodeScanned}
+              barcodeScannerSettings={{
+                barcodeTypes: ["qr", "pdf417"],
+              }}
+              // barcodeScannerSettings={{}} 
+              facing={facing}>
+              <View style={{
+                  flex: 1,
+                  flexDirection: 'row',
+                  backgroundColor: 'transparent',
+                  margin: 64,
+              }}>
+                  {/* Area to add content on the screen of the camera  */}
+              {/* <TouchableOpacity
+              style={{
+                  flex: 1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+              }}
+              //  onPress={}
+              >
+                  <Text style={{
+                        fontSize: 24,
+                        fontWeight: 'bold',
+                        color: 'white',
+                  }}>Flip Camera</Text>
+              </TouchableOpacity> */}
+              </View>
+          </CameraView>
+          <View style={{
+              width: "100%",
+              height: "20%",
+              display: "flex", 
+              justifyContent: "center"
+              // alignItems:"center"
+
+          }}>
+              <Text style={{
+                  color: "white", 
+                  textAlign: "center"
+              }}>Scan your friends QR code</Text>
+          </View>
+            
+           </View>
+
+        }
+        
+          </View> 
+
+    )})
 
 export default QrCodeContent
--- component/search_component/searchpageStructure.tsx
+++ component/search_component/searchpageStructure.tsx
@@ -1,143 +1,170 @@
-import React, { useState } from 'react'
-import { View, Text, StyleSheet, TouchableOpacity, TextInput} from 'react-native'
-import AddFriendsIcon from '../../icons/addFriendsIcon'
-import PuzzleIcon from '../../icons/puzzleIcon'
-import OutlineScanIcon from '../../icons/outlineScanIcon'
-import SearchLoopIcon from '../../icons/searchLoopIcon'
-import SearchContent from './BelowContentSection/searchContent'
-import QrCodeContent from './BelowContentSection/qrContent'
-import SearchMenuToggleContent from './searchMenu'
-import SearchNQRCodeBar from './UpperContentSection/SearchNQRCodeBar'
-import SearchOption from './UpperContentSection/searchOption'
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+
+import SearchContent from "./BelowContentSection/searchContent";
+import QrCodeContent from "./BelowContentSection/qrContent";
+import SearchMenuToggleContent from "./searchMenu";
+import SearchOption from "./UpperContentSection/searchOption";
+import { Button } from "react-native";
+import { useRef } from "react";
+
+
+
+
 
 export default function SearchpageStructure() {
-    const [searchMenuToggle, setSearchMenuToggle] = useState(false)
-    const [addUserToggle, setAddUserToggle] = useState(true)
-    const [QRCodeToggle, setQrCodeToggle] = useState(false)
-    const [inputConditions, setInputConditions] = useState(true)
-    const [inputValue, setInputValue] = useState("")
-    const [searchContent, setSearchContent] = useState(true)
-    const [qrCodeContent, setQrCodeContent] = useState(false)
+  const [searchMenuToggle, setSearchMenuToggle] = useState(false);
+  const [addUserToggle, setAddUserToggle] = useState(true);
+  const [QRCodeToggle, setQrCodeToggle] = useState(false);
+  const [inputConditions, setInputConditions] = useState(true);
+  const [inputValue, setInputValue] = useState("");
+  const [searchContent, setSearchContent] = useState(true);
+  const [qrCodeContent, setQrCodeContent] = useState(false);
+  const [foundUser, setFoundUser] = useState(false)
+  const [barCodeType, setbarCodeType] = useState(null)
+  const [barCodeData, setBarCodeData] = useState(null)
+  const childRef = useRef<any>(null);
+
+  const handleToggleSearchMenu = () => {
+    setSearchMenuToggle(!searchMenuToggle);
+  };
 
 
-    const handleToggleSearchMenu = () => {
-        setSearchMenuToggle(!searchMenuToggle)
+
+
+  const handleUserisFound = ({type, data}: any) => {
+    console.log('User is founded');
+    console.log('handleUserisFound Data: ', data);
+    console.log('handleUserisFound type: ', type);
+    setFoundUser(true)
+    setbarCodeType(type)
+    setBarCodeData(data)
+
+  
+    
+
+  }
+
+  const handleQCloseEvent = () => {
+    setFoundUser(false)
+    if(childRef.current){
+      childRef.current.handleAkivateScann();
     }
+  }
+  const handleQRToggle = () => {
+    setAddUserToggle(false);
+    setSearchContent(false);
+    setQrCodeToggle(true);
+    setQrCodeContent(true);
+    setInputConditions(false);
+    setSearchMenuToggle(false);
+    setInputValue("");
+  };
 
-    const handleQRToggle = () => {
-
-        console.log('qr code toggle ');
-        setAddUserToggle(false)
-        setSearchContent(false)
-        setQrCodeToggle(true)
-        setQrCodeContent(true)
-        setInputConditions(false)
-        setSearchMenuToggle(false)
-        setInputValue("")
-
-
-    }
-
-    const handleUserToggle = () => {
-        
-        setQrCodeToggle(false)
-        setQrCodeContent(false)
-        setAddUserToggle(true)
-        setSearchContent(true)
-        setInputConditions(true)
-        setSearchMenuToggle(false)
-    }
+  const handleUserToggle = () => {
+    setQrCodeToggle(false);
+    setQrCodeContent(false);
+    setAddUserToggle(true);
+    setSearchContent(true);
+    setInputConditions(true);
+    setSearchMenuToggle(false);
+  };
   return (
-    <View style={ {
+    <View
+      style={{
         height: "100%",
-    }}>
-
-        {/* Upper part:  Search Section, user can search through other user name or use the QR Code methode */}
-        <View style={{
-            height: "15%",
-            // backgroundColor: "orange",
-            alignItems: "center",
-            padding: 5,
-            flexDirection: "row",
-            // gap: 10
-
-
-
-        }}>
-           
-
-           
-            <SearchOption
-            QRCodeToggle = {QRCodeToggle}
-            addUserToggle ={addUserToggle}
-            handleToggleSearchMenu = {handleToggleSearchMenu}
-            setInputValue={setInputValue}
-            inputValue={inputValue}
-            inputConditions={inputConditions}
-            
-            />
-
-            
+      }}
+    >
+      {
+          foundUser 
+          &&
+          <View style={{
+            width: "100%", 
+            height: "100%",
+            // backgroundColor: "pink", 
+            backgroundColor: "rgba(0, 0, 0, 0.4)", 
+            // flex: "100%", 
+            // position: "absolute",
+            zIndex: 3
 
 
-            { searchMenuToggle  &&
-            (
-                
-                <SearchMenuToggleContent 
-                QRCodeToggle={QRCodeToggle}
-                addUserToggle={addUserToggle}
-                handleQRToggle={handleQRToggle} 
-                handleUserToggle={handleUserToggle} />
-            )}
-           
-        </View>
+          }}>
+              <Text style={{color: "white"}}>Founded type: {barCodeType}</Text>
+              <Text style={{color: "white"}}>Founded Data: {barCodeData}</Text>
+              <Button title="close" onPress={handleQCloseEvent}></Button>
+          </View>
+         
+          }
+    
 
-        {/* Below part: Displays the choosen Method content & Result */}
-        <View style={{
-             height: "85%",
-             zIndex: -1,
-             backgroundColor: "green",
-             alignItems: "center",
-             padding: 10,
-             flexDirection: "row",
-             // gap: 10
-        }}>
+    
 
-            {
-                QRCodeToggle ?
-                // QR code Content
-                
-                <QrCodeContent/>
+    
+    
+      {/* Upper part:  Search Section, user can search through other user name or use the QR Code methode */}
+      <View
+        style={{
+          height: "15%",
+          // backgroundColor: "orange",
+          alignItems: "center",
+          padding: 5,
+          flexDirection: "row",
+          // gap: 10
+        }}
+      >
+        {/* Displays the Option that the user can use to search for other users  */}
+        <SearchOption
+          QRCodeToggle={QRCodeToggle}
+          addUserToggle={addUserToggle}
+          handleToggleSearchMenu={handleToggleSearchMenu}
+          setInputValue={setInputValue}
+          inputValue={inputValue}
+          inputConditions={inputConditions}
+        />
+            {/* Menu for selecting the search option */}
+        {searchMenuToggle && (
+          <SearchMenuToggleContent
+            QRCodeToggle={QRCodeToggle}
+            addUserToggle={addUserToggle}
+            handleQRToggle={handleQRToggle}
+            handleUserToggle={handleUserToggle}
+          />
+        )}
+      </View>
 
-                : 
-                // Search Content
-
-                <SearchContent/>
-                
-
-            }
-           
-
-
-        </View>
+      {/* Below part: Displays the choosen Method content & Result */}
+      <View
+        style={{
+          height: "85%",
+          zIndex: -1,
+          // backgroundColor: "green",
+          alignItems: "center",
+          padding: 10,
+          flexDirection: "row",
+          // gap: 10
+        }}
+      >
+          
         
-      
+
+
+        {QRCodeToggle ? (
+          // QR code Content
+
+          <QrCodeContent ref={childRef}  handleUserisFound={handleUserisFound} />
+        ) : (
+          // Search Content
+
+          <SearchContent />
+        )}
+      </View>
     </View>
-  )
+  );
 }
 
-
-
-// const styles = StyleSheet.create({
-//     container: {
-//     //   backgroundColor: "red",
-//     //   flex: 1,
-//      height: "100%",
-//     //   justifyContent: "center",
-//     //   paddingTop: 50,
-
-//     },
- 
- 
-
-// })
