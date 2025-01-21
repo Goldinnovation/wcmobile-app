7f34c06ef66bb5b063968637807d7615895e4d0e
Goldinnovation <emanuelneuenhaus@hotmail.com>
2025-01-21 11:55:35 +0100

    removed mapbox due to native code problems

--- navigation/modeScreen.tsx
+++ navigation/modeScreen.tsx
@@ -6,9 +6,7 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { ParamListBase } from "@react-navigation/native";
-import Mapbox from '@rnmapbox/maps';
 
-Mapbox.setAccessToken("pk.eyJ1IjoiZ29sZGlubm92YXRpb24iLCJhIjoiY202NmJpaGppMXljdjJscjBpZzVxZXJtYyJ9.7xZ3FKUVTmYl1YL2QrsW4Q")
 
 
 const ModeScreen = () => {
@@ -39,9 +37,7 @@ const ModeScreen = () => {
             alignItems: 'center',
 
            }}>
-              <Mapbox.MapView style={{
-                flex: 1
-              }} />
+             
            </View>
         </View>
     )
